ba04a759766e12575373f8f0998cf32cb6160657
ziu7wx <jin.zhu5@bosch.com>
2023-02-04 17:20:36 +0800

    feat: log management for agent

--- core/log.py
+++ core/log.py
@@ -5,14 +5,14 @@ from lib import utils
 class AgentLogger:
     def __init__(self, queue):
         self.queue = queue
-        self.host = utils.get_host_name()
+        self.hostname = utils.get_host_name()
 
     def remote_handler(self, message: str):
         data = {
             "purpose": "log",
             "method": "agent_log_message",
             "message": {
-                "host": self.host,
+                "hostname": self.hostname,
                 "content": message.strip()
             }
         }
